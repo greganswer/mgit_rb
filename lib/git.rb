b899b67f9723602f9bfdce8d24017dc86f045868
Greg Answer <greganswer@gmail.com>
2020-03-06 18:43:59 -0500

    Implement additional methods

--- lib/git.rb
+++ lib/git.rb
@@ -1,43 +1,54 @@
-require "open3" 
-# Ref: https://makandracards.com/makandra/44452-running-external-commands-with-open3
+# frozen_string_literal: true
 
+# Ref: https://makandracards.com/makandra/44452-running-external-commands-with-open3
+require 'open3'
+
+# Git contains functions that execute git commands.
 module Git
-  def self.branch_exists(name)
-    puts "TODO: Implement Git.branch_exists"
+  def self.branch_exists(_name)
+    puts 'TODO: Implement Git.branch_exists'
     true
   end
 
-  def self.create_branch(base_branch, new_branch)
-    puts "TODO: Implement Git.create_branch"
+  def self.create_branch(_base_branch, _new_branch)
+    puts 'TODO: Implement Git.create_branch'
   end
 
   def self.current_branch
-    Open3.capture3("git", "rev-parse", "--abbrev-ref", "HEAD")
+    stdout, stderr, status = Open3.capture3('git', 'rev-parse', '--abbrev-ref', 'HEAD')
+    [stdout.strip, stderr, status]
   end
 
-  def self.commit_all(message)
-    puts "TODO: Implement Git.commit_all"
+  def self.commit_all(_message)
+    puts 'TODO: Implement Git.commit_all'
   end
 
   def self.default_base_branch
-    puts "TODO: Implement Git.default_base_branch"
+    puts 'TODO: Implement Git.default_base_branch'
     # %w(dev develop development master)
-    "master"
+    'master'
   end
 
   def self.init
-    puts "TODO: Implement Git.init"
+    puts 'TODO: Implement Git.init'
   end
 
-  def self.pull_request(base_branch, body)
-    puts "TODO: Implement Git.pull_request"
+  def self.initialized?
+    File.exist?('.git')
   end
 
-  def self.push(branch=None)
-    puts "TODO: Implement Git.push"
+  def self.pull_request(_input)
+    puts 'TODO: Implement Git.pull_request'
+    # TODO: Validate input
+    # input[:issue]
+    # input[:base_branch]
   end
 
-  def self.rebase(base_branch)
-    puts "TODO: Implement Git.rebase"
+  def self.push(_branch = None)
+    puts 'TODO: Implement Git.push'
+  end
+
+  def self.rebase(_base_branch)
+    puts 'TODO: Implement Git.rebase'
   end
 end
--- lib/issue.rb
+++ lib/issue.rb
@@ -1,18 +1,21 @@
+# frozen_string_literal: true
+
+# Issue stores the data for the current issue.
 class Issue
   attr_accessor :id
 
   def self.from_branch(branch)
     puts "TODO: Implement Issue.from_branch for #{branch} branch"
-    id = "JIR-123"
-    summary = "Fake long title"
-    self.new(id: id, summary: summary)
+    id = 'JIR-123'
+    summary = 'Fake long title'
+    new(id: id, summary: summary)
   end
 
   def self.from_tracker(issue_id)
-    puts "TODO: Implement Issue.from_tracker"
+    puts 'TODO: Implement Issue.from_tracker'
     id = issue_id
-    summary = "Fake long title"
-    self.new(id: id, summary: summary)
+    summary = 'Fake long title'
+    new(id: id, summary: summary)
   end
 
   def initialize(args = {})
@@ -21,26 +24,24 @@ class Issue
   end
 
   def to_s
-    self.id && self.title ? "#{id}: #{title}" : title
+    id && title ? "#{id}: #{title}" : title
   end
 
   def title
-    puts "TODO: Implement issue.title"
-    summary
+    summary.split(' ').map(&:capitalize).join(' ')
   end
 
   def branch_name
-    puts "TODO: Implement issue.branch_name"
-    "jir-123-fake-branch-name"
+    "#{id}-#{title}".downcase.strip.gsub(/[^a-z0-9\-_]+/i, '-')
   end
 
   def api_url
-    puts "TODO: Implement issue.branch_name"
+    puts 'TODO: Implement issue.branch_name'
     "https://api.example.com/issues/#{id}"
   end
 
   def web_url
-    puts "TODO: Implement issue.web_url"
+    puts 'TODO: Implement issue.web_url'
     "https://example.com/issues/#{id}"
   end
 
