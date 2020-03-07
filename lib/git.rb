398c9295b08f417023e010a09a06c40f803b3b88
Greg Answer <greganswer@gmail.com>
2020-03-07 00:03:30 -0500

    Refactor to rubocop standards

--- lib/git.rb
+++ lib/git.rb
@@ -5,49 +5,62 @@ require 'open3'
 
 # Git contains functions that execute git commands.
 module Git
-  def self.branch_exists(_name)
-    puts 'TODO: Implement Git.branch_exists'
-    true
+  # Check if the branch exists in the current Git repo.
+  def self.branch_exists(name)
+    _, _, status = Open3.capture3('git', 'rev-parse', '--quiet', '--verify', name.to_s)
+    status.success?
   end
 
+  # TODO: Handle errors, same as Git.current_branch()
   def self.create_branch(_base_branch, _new_branch)
     puts 'TODO: Implement Git.create_branch'
   end
 
+  # Get the current branch name or raise an error.
   def self.current_branch
     stdout, stderr, status = Open3.capture3('git', 'rev-parse', '--abbrev-ref', 'HEAD')
-    [stdout.strip, stderr, status]
+    raise stderr unless status.success?
+
+    stdout.strip
   end
 
+  # TODO: Handle errors, same as Git.current_branch()
   def self.commit_all(_message)
     puts 'TODO: Implement Git.commit_all'
   end
 
   def self.default_base_branch
-    puts 'TODO: Implement Git.default_base_branch'
-    # %w(dev develop development master)
-    'master'
+    branches = %w[dev develop development master]
+    branches.each do |name|
+      return name if branch_exists(name)
+    end
+    raise "Could not find any of the following base branches: #{branches}"
   end
 
+  # Initialize a git repository.
   def self.init
-    puts 'TODO: Implement Git.init'
+    _, stderr, status = Open3.capture3('git', 'init')
+    raise stderr unless status.success?
   end
 
   def self.initialized?
     File.exist?('.git')
   end
 
+  # TODO: Handle errors, same as Git.current_branch()
   def self.pull_request(_input)
     puts 'TODO: Implement Git.pull_request'
     # TODO: Validate input
-    # input[:issue]
-    # input[:base_branch]
+    # input[:issue].to_s
+    # input[:base_branch].to_s
   end
 
-  def self.push(_branch = None)
+  # TODO: Handle errors, same as Git.current_branch()
+  def self.push(_branch)
     puts 'TODO: Implement Git.push'
   end
 
+  # TODO: Handle errors, same as Git.current_branch()
   def self.rebase(_base_branch)
     puts 'TODO: Implement Git.rebase'
   end
--- lib/issue.rb
+++ lib/issue.rb
@@ -4,16 +4,23 @@
 class Issue
   attr_accessor :id
 
-  def self.from_branch(branch)
-    puts "TODO: Implement Issue.from_branch for #{branch} branch"
-    id = 'JIR-123'
-    summary = 'Fake long title'
-    new(id: id, summary: summary)
+  # Create an issue from the branch name. Uses O(n) time and space.
+  def self.from_branch(name)
+    parts = name.to_s.split('-')
+    parts.each_with_index do |value, index|
+      next unless value.to_i.to_s == value
+
+      id = parts[0..index].join('-').upcase
+      summary = parts[index + 1..-1].join(' ')
+      return new(id: id, summary: summary)
+    end
+    new
   end
 
+  # Create an issue by making an HTTP request to the issue tracker API.
   def self.from_tracker(issue_id)
     puts 'TODO: Implement Issue.from_tracker'
-    id = issue_id
+    id = issue_id.to_s.upcase
     summary = 'Fake long title'
     new(id: id, summary: summary)
   end
@@ -31,12 +38,13 @@ class Issue
     summary.split(' ').map(&:capitalize).join(' ')
   end
 
+  # Get the branch name from ID and title.
   def branch_name
     "#{id}-#{title}".downcase.strip.gsub(/[^a-z0-9\-_]+/i, '-')
   end
 
   def api_url
-    puts 'TODO: Implement issue.branch_name'
+    puts 'TODO: Implement issue.api_url'
     "https://api.example.com/issues/#{id}"
   end
 
